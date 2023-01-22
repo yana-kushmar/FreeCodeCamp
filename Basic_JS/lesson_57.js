08b54d6bdba110a34f21940a2858dc5a1840e90d
yana-kushmar <yana.puzevich@gmail.com>
2023-01-22 05:03:34 +0300

    Stand in Line

---
+++ Basic_JS/lesson_57.js
@@ -0,0 +1,11 @@
+function nextInLine(arr, item) {
+    arr.push(item);
+    return arr.shift();
+}
+
+let testArr = [1, 2, 3, 4, 5];
+
+// Display code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
