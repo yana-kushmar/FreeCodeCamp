025ddd60393800bba2b35e8f82dccf5fcb5d03b9
yana-kushmar <yana.puzevich@gmail.com>
2023-01-25 17:13:58 +0300

    Use Destructuring Assignment to Assign Variables from Objects

---
+++ ES6/lesson_10.js
@@ -0,0 +1,11 @@
+const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+};
+
+
+
+const {today: highToday} = HIGH_TEMPERATURES;
+const {tomorrow: highTomorrow } = HIGH_TEMPERATURES;
+
